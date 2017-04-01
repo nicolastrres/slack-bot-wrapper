afc358aeaa8f4a1874d1ff26a87f74a6992a38d9
Nicolas Agustin Torres <nicolastrres@gmail.com>
2017-04-01 19:25:55 -0300

    Add command handlers

--- slack_bot/slack_bot.py
+++ slack_bot/slack_bot.py
@@ -1,14 +1,37 @@
 
 class SlackBot:
-    def __init__(self, bot_name, client):
+    def __init__(self, bot_name, client, commands_handlers=None):
         self.client = client
         self.bot_name = bot_name
+        self.commands_handlers = commands_handlers or {}
 
-    def _get_users(self):
-        api_call = self.client.api_call('users.list')
-        return api_call.get('members') if api_call.get('ok') else []
+    def _unknown_command(self, channel):
+        commands = ', '.join(self.commands)
+
+        response = 'Not sure what you mean. ' \
+                   'Available commands are: %s' % commands
+        self.client.post_message(
+            channel=channel,
+            message=response,
+            as_user=True
+        )
+
+    @property
+    def commands(self):
+        return sorted([command for command in self.commands_handlers.keys()])
 
     @property
     def id(self):
-        for user in self._get_users():
-            return user.get('id') if user.get('name') == self.bot_name else None
+        for user in self.client.get_users():
+            return user.get('id') if user.get(
+                'name') == self.bot_name else None
+
+    @property
+    def at_bot(self):
+        return '<@' + self.id + '>'
+
+    def handle_command(self, command, channel):
+        if command in self.commands:
+            self.commands_handlers[command]()
+        else:
+            self._unknown_command(channel)
--- tests/test_slack_bot.py
+++ tests/test_slack_bot.py
@@ -1,32 +1,102 @@
 import unittest
+from functools import partial
 from unittest.mock import Mock
 
 from slack_bot.slack_bot import SlackBot
 
 
 class SlackBotTest(unittest.TestCase):
-    def test_get_bot_id_successfully(self):
-        users = [
+    def setUp(self):
+        self.users = [
             {'id': 'some-id', 'name': 'bot-name'},
             {'id': 'another-id', 'name': 'another-name'}
         ]
-        api_call_response = {'ok': True, 'members': users}
-        slack_client_mocked = Mock()
-        slack_client_mocked.api_call.return_value = api_call_response
+        self.slack_client_mocked = Mock()
 
-        slack_bot = SlackBot('bot-name', slack_client_mocked)
+    def test_get_bot_id_successfully(self):
+        self.slack_client_mocked.get_users.return_value = self.users
+        slack_bot = SlackBot('bot-name', self.slack_client_mocked)
 
         self.assertEqual(slack_bot.id, 'some-id')
 
     def test_get_bot_id_wrong_name(self):
-        users = [
-            {'id': 'some-id', 'name': 'bot-name'},
-            {'id': 'another-id', 'name': 'another-name'}
-        ]
-        api_call_response = {'ok': True, 'members': users}
-        slack_client_mocked = Mock()
-        slack_client_mocked.api_call.return_value = api_call_response
+        self.slack_client_mocked.get_users.return_value = self.users
 
-        slack_bot = SlackBot('wrong-bot-name', slack_client_mocked)
+        slack_bot = SlackBot('wrong-bot-name', self.slack_client_mocked)
 
         self.assertIsNone(slack_bot.id)
+
+    def test_at_bot(self):
+        self.slack_client_mocked.get_users.return_value = self.users
+
+        slack_bot = SlackBot('bot-name', self.slack_client_mocked)
+
+        self.assertEqual(slack_bot.at_bot, '<@some-id>')
+
+    def test_handle_command_calling_right_handler(self):
+        mocked_command = Mock()
+        command_handlers = {
+            'help': mocked_command,
+            'another_command': Mock()
+        }
+
+        slack_bot = SlackBot(
+            'bot-name',
+            self.slack_client_mocked,
+            commands_handlers=command_handlers
+        )
+
+        slack_bot.handle_command('help', 'some_channel')
+        self.assertTrue(mocked_command.called)
+
+    def test_handle_command_calling_right_handler_with_args(self):
+        mocked_command = Mock()
+        command_handlers = {
+            'help': partial(mocked_command, 'some-arg'),
+            'another_command': Mock()
+        }
+
+        slack_bot = SlackBot(
+            'bot-name',
+            self.slack_client_mocked,
+            commands_handlers=command_handlers
+        )
+
+        slack_bot.handle_command('help', 'some_channel')
+        mocked_command.assert_called_once_with('some-arg')
+
+    def test_handle_command_show_available_commands_when_not_existent_command(
+            self
+    ):
+        command_handlers = {
+            'help': Mock(),
+            'another_command': Mock()
+        }
+        expected_message = 'Not sure what you mean. ' \
+                           'Available commands are: another_command, help'
+
+        slack_bot = SlackBot(
+            'bot-name',
+            self.slack_client_mocked,
+            commands_handlers=command_handlers
+        )
+
+        slack_bot.handle_command('not-existent-command', 'some-channel')
+        self.slack_client_mocked.post_message.assert_called_once_with(
+            channel='some-channel',
+            message=expected_message,
+            as_user=True
+        )
+
+    def test_get_sorted_commands(self):
+        command_handlers = {
+            'help': Mock(),
+            'another_command': Mock()
+        }
+
+        slack_bot = SlackBot(
+            'bot-name',
+            self.slack_client_mocked,
+            commands_handlers=command_handlers
+        )
+        self.assertEqual(slack_bot.commands, ['another_command', 'help'])
