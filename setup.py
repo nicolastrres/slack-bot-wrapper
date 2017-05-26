1c58fbae9b2d89118e321080bb62a4449a30eff2
Nicolas Agustin Torres <nicolastrres@gmail.com>
2017-05-26 13:48:58 -0300

    Release a new version with the latest changes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='slack-bot-wrapper',
-      version='0.0.2',
+      version='0.0.3',
       description='Slack Bot Wrapper',
       long_description='Allows to create Slack bots and add features to them '
                        'very easily.',
